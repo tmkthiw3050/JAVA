ce296637b884c04119bbb467434de73606723d32
tmkthiw3050 <116328575+tmkthiw3050@users.noreply.github.com>
2022-10-21 11:07:18 +0700

    tmkthiw
    55555555555555555555

--- Basic/Hello.java
+++ Basic/Hello.java
@@ -1,5 +1,7 @@
 public class Hello{
                     public static void main(String[] args) {
                                         System.out.println("HelloWorld!");
+                                        System.out.println("myname is thiw");
+                                        System.out.println("56");
                     }
 }
